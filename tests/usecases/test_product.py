21b067aed04fd88c5511ce9effa41b923aa698d6
Eber Brea <88146419+ebrea@users.noreply.github.com>
2025-07-21 15:06:56 -0300

    Update test_product.py

--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -11,14 +11,14 @@ async def test_usecases_create_should_return_success(product_in):
     result = await product_usecase.create(body=product_in)
 
     assert isinstance(result, ProductOut)
-    assert result.name == "Iphone 14 Pro Max"
+    assert result.name == "Iphone 16 Pro Max"
 
 
 async def test_usecases_get_should_return_success(product_inserted):
     result = await product_usecase.get(id=product_inserted.id)
 
     assert isinstance(result, ProductOut)
-    assert result.name == "Iphone 14 Pro Max"
+    assert result.name == "Iphone 16 Pro Max"
 
 
 async def test_usecases_get_should_not_found():
